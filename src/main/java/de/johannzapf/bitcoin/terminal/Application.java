3b8ff73bae100295f5708c9ea9e2bcb12b574478
Johann Theo Zapf <jozapf@mail.uni-mannheim.de>
2021-11-10 15:06:41 +0100

    first pin, then initialize
    (cherry picked from commit e84712b198bf89621331cd53596579d4fe1dbdbc)

--- src/main/java/de/johannzapf/bitcoin/terminal/Application.java
+++ src/main/java/de/johannzapf/bitcoin/terminal/Application.java
@@ -82,10 +82,10 @@ public class Application {
         } while (true);
 
         if(!cardStatus(channel)){
-            System.out.println("Initializing Bitcoin Wallet on this card...");
-            initializeWallet(channel);
             System.out.println("Please set a PIN on the smart card reader");
             PINService.modifyPin(card);
+            System.out.println("Initializing Bitcoin Wallet on this card...");
+            initializeWallet(channel);
         }
 
         if(connectionMode){
